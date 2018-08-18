87f2049478b369e6114a951d8e833753642fbd50
alish9776 <ali.76sh.97@gmail.com>
2018-08-18 10:38:11 +0430

    added todo app with es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: './styles/custom.scss',
+    entry: {
+        script: ['./app.js', './styles/custom.scss']
+    },
     output: {
-        filename: 'styles/global/custom.css'
+        path:path.join(__dirname, "dist"),
+        filename: '[name].js'
     },
     module: {
-
         rules: [
-            /*
-            your other rules for JavaScript transpiling go in here
-            */
-            { // regular css files
-                test: /\.css$/,
+            {
+                test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    use: 'css-loader?importLoaders=1',
-                }),
-            },
-            { // sass / scss loader for webpack
-                test: /\.(sass|scss)$/,
-                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+                    fallback: "style-loader",
+                    use: ['css-loader', 'sass-loader']
+                })
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin({ // define where to save the file
-            filename: 'styles/global/custom.css',
-            allChunks: true,
-        }),
-    ],
-};
+        new ExtractTextPlugin('bundle.css')
+    ] };
